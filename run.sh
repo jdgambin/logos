3b38123486187a8ed5a361a6f75fac9dd628c686
jdgambin <jgambinalvarez@correo.unicordoba.edu.co>
2023-05-30 11:10:22 -0500

    agregar script para ejecutar la aplicación web

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ! type -P flask &> /dev/null
+then
+	printf "The Flask framework isn't installed.\n" >&2
+	printf "Install the following dependencies:\n" >&2
+	printf "> pip install flask flask-wtf python-dotenv\n" >&2
+	exit 1
+fi
+
+export FLASK_APP=logos.py
+flask run --debug
